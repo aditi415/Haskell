eeec8e12724c823c6ecc0978bb6e7dc3155ba610
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-04-28 13:23:07 +0530

    Add files via upload

---
+++ addition.hs
@@ -0,0 +1,10 @@
+addnumbers :: Int -> Int -> Int
+addnumbers a b = a + b
+
+main :: IO ()
+main do
+    let a = 5
+    let b = 7
+    let result = addnumbers a b
+    putStrLn ("Addition of numbers is:" ++ show result)
+       
