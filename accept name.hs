97eb2759229de7c439e9d503a1532dafe8b84988
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-05-10 21:40:41 +0530

    accept name.hs

--- accept name.hs
+++ accept name.hs
@@ -2,4 +2,4 @@ main :: IO ()
 main do
     putStrLn "What is your name? "          --name is accepted using  putStrLn
     name <- getLine                         --input is accepted using getLine and store in name variable
-    putStrLn ("Hello, " ++ name ++ "!")     --prints the name on console 
+    putStrLn ("Hello, " ++ name ++ "!")     --prints the name on console
